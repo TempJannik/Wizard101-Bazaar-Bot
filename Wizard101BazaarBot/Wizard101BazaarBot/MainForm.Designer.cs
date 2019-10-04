060b422f069b2b6b7aeb608f9e78051dadcbf276
Jannik P <30756486+Tempodoa@users.noreply.github.com>
2019-10-04 11:19:47 +0200

    first UI concepts

--- Wizard101BazaarBot/Wizard101BazaarBot/MainForm.Designer.cs
+++ Wizard101BazaarBot/Wizard101BazaarBot/MainForm.Designer.cs
@@ -30,44 +30,199 @@
         {
             this.availableBox = new System.Windows.Forms.ListBox();
             this.selectedBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.xLabel = new System.Windows.Forms.Label();
+            this.yLabel = new System.Windows.Forms.Label();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // availableBox
             // 
             this.availableBox.FormattingEnabled = true;
-            this.availableBox.Items.AddRange(new object[] {
-            "dsad",
-            "dasda",
-            "dasdad"});
-            this.availableBox.Location = new System.Drawing.Point(364, 62);
+            this.availableBox.Location = new System.Drawing.Point(445, 32);
             this.availableBox.Name = "availableBox";
-            this.availableBox.Size = new System.Drawing.Size(120, 95);
+            this.availableBox.Size = new System.Drawing.Size(120, 121);
             this.availableBox.TabIndex = 0;
             this.availableBox.SelectedIndexChanged += new System.EventHandler(this.AvailableBox_SelectedIndexChanged);
             // 
             // selectedBox
             // 
             this.selectedBox.FormattingEnabled = true;
-            this.selectedBox.Items.AddRange(new object[] {
-            "dsad",
-            "dasda",
-            "dasdad"});
-            this.selectedBox.Location = new System.Drawing.Point(579, 62);
+            this.selectedBox.Location = new System.Drawing.Point(571, 32);
             this.selectedBox.Name = "selectedBox";
-            this.selectedBox.Size = new System.Drawing.Size(120, 95);
+            this.selectedBox.Size = new System.Drawing.Size(120, 121);
             this.selectedBox.TabIndex = 1;
             this.selectedBox.SelectedIndexChanged += new System.EventHandler(this.SelectedBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(442, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Possible Reagents:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(568, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Monitored Reagents:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Available Gold: Unknown";
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(145, 390);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(127, 44);
+            this.StartBtn.TabIndex = 5;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Location = new System.Drawing.Point(278, 390);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(127, 44);
+            this.StopBtn.TabIndex = 6;
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(445, 192);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(246, 242);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(442, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Log:";
+            // 
+            // xLabel
+            // 
+            this.xLabel.AutoSize = true;
+            this.xLabel.Location = new System.Drawing.Point(9, 90);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(26, 13);
+            this.xLabel.TabIndex = 9;
+            this.xLabel.Text = "X: 0";
+            // 
+            // yLabel
+            // 
+            this.yLabel.AutoSize = true;
+            this.yLabel.Location = new System.Drawing.Point(9, 103);
+            this.yLabel.Name = "yLabel";
+            this.yLabel.Size = new System.Drawing.Size(26, 13);
+            this.yLabel.TabIndex = 10;
+            this.yLabel.Text = "Y: 0";
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(12, 390);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(127, 44);
+            this.SaveBtn.TabIndex = 11;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Reagent Position";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "X: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 211);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 14;
+            this.textBox1.Text = "507";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(172, 211);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.Text = "273";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(146, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Y: ";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(711, 446);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.yLabel);
+            this.Controls.Add(this.xLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.selectedBox);
             this.Controls.Add(this.availableBox);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "BazaarBot";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,6 +230,21 @@
 
         private System.Windows.Forms.ListBox availableBox;
         private System.Windows.Forms.ListBox selectedBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.Label yLabel;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- Wizard101BazaarBot/Wizard101BazaarBot/MainForm.cs
+++ Wizard101BazaarBot/Wizard101BazaarBot/MainForm.cs
@@ -1,34 +1,73 @@
-﻿using System;
+﻿using Google.Cloud.Vision.V1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Wizard101BazaarBot
 {
+    
+
     public partial class MainForm : Form
     {
         public MainForm()
         {
             InitializeComponent();
+            /*Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"C:\Users\Jannik\Downloads\My First Project-5ac39e1eeea6.json");
+            Google.Cloud.Vision.V1.Image img1 = Google.Cloud.Vision.V1.Image.FromFile(@"C:\Users\Jannik\Desktop\2019-10-01 18_36_21-Wizard101.png");
+            ImageAnnotatorClient client = ImageAnnotatorClient.Create();
+            IReadOnlyList<EntityAnnotation> textAnnotations = client.DetectLabels(img1);
+            foreach (EntityAnnotation text in textAnnotations)
+            {
+                Console.WriteLine($"Description: {text.Description}");
+            }*/
+            Control.CheckForIllegalCrossThreadCalls = false;
+            new Thread(() => UpdateMouse()).Start();
+        }
+
+        private void UpdateMouse()
+        {
+            while(true)
+            {
+                xLabel.Text = "X: "+Cursor.Position.X;
+                yLabel.Text = "Y: "+Cursor.Position.Y;
+                Thread.Sleep(10);
+            }
         }
 
         private void AvailableBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string currItem = availableBox.GetItemText(availableBox.SelectedItem);
-            availableBox.Items.RemoveAt(availableBox.SelectedIndex);
-            selectedBox.Items.Add(currItem);
+            if(availableBox.GetItemText(availableBox.SelectedItem) != "")
+            {
+                string currItem = availableBox.GetItemText(availableBox.SelectedItem);
+                availableBox.Items.RemoveAt(availableBox.SelectedIndex);
+                selectedBox.Items.Add(currItem);
+            }
+            
         }
 
         private void SelectedBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string currItem = selectedBox.SelectedItem.ToString();
-            selectedBox.Items.RemoveAt(selectedBox.SelectedIndex);
-            availableBox.Items.Add(currItem);
+            if(selectedBox.GetItemText(selectedBox.SelectedItem) != "")
+            {
+                string currItem = selectedBox.GetItemText(selectedBox.SelectedItem);
+                selectedBox.Items.RemoveAt(selectedBox.SelectedIndex);
+                availableBox.Items.Add(currItem);
+            }
         }
+    }
+
+    public class RootObject
+    {
+        public List<string> availableReagents { get; set; }
+        public List<string> selectedReagents { get; set; }
+        public int reagentsX { get; set; }
+        public int reagentsY { get; set; }
     }
 }
